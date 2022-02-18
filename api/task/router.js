c2921ce513792ee252e723ace5effb5788e9e3f3
Allison Stewart <amstewart@yahoo.com>
2022-02-17 22:57:29 -0500

    server, index.js, and routers connected

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,15 @@
 // build your `/api/tasks` router here
+const router = require('express').Router()
+
+router.use('/', (req, res) => {
+    res.json({ tasksApi: 'up'})
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+require('dotenv').config()
+
+const server = require('./api/server');
+
+const PORT = process.env.PORT
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
